89c42349762711821f97a39f54aa9b5828555d37
JoulePhi <dzulfikar.sadid.khoir@gmail.com>
2024-02-01 17:58:13 +0700

    Final 1

--- course.py
+++ course.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 from tkinter import ttk,messagebox, Toplevel
 
-#Array :
 
-# 1. List Kelas
+# List kelas yang tersedia
+# Nama kelas, nama mentor, durasi (bulan)
 kelas = [
     ["Data Science", "Jane Smith", 3],
     ["Web Development", "Alex Johnson", 5],
@@ -15,14 +15,29 @@ kelas = [
     ["React Native", "John Doe", 4],
 ]
 
-# 2. List User
-user = []    
-# 3. List User Kelas
-user_kelas = []
+# List user yang terdaftar
+# Nama, email, nomor telepon
+user = [
+    ['Dzulfikar Sadid','dzulfikar@mail.com','08123456789'],
+    ['Ahmad Riski','ahmad@mail.com','08123456789'],
+    ['Naufal Azka','naufal@mail.com','08123456789'],
+]   
 
+# List user yang terdaftar di kelas
+# Nama, kelas
+user_kelas = [
+    ['Dzulfikar Sadid','Data Science'],
+    ['Ahmad Riski','Web Development'],
+    ['Naufal Azka','Machine Learning'],
+    ['Dzulfikar Sadid','React Native'],
+    ['Ahmad Riski','Backend Development'],
+    ['Naufal Azka','Backend Development'],
+]
 
+# List terurut ascending atau tidak
+is_acending = True
 
-
+# Inisialisai GUI
 root = tk.Tk()
 root.title("Bootcamp Online")
 root.geometry("1280x720")  
@@ -54,46 +69,59 @@ def compare_user_descending(a, b):
     return a[0] < b[0]
 
 def sort_and_refresh(lst, compare, refresh):
+    global is_acending
+    if compare == compare_kelas_ascending or compare == compare_user_ascending:
+        is_acending = True
+    else:
+        is_acending = False
+
     bubble_sort(lst, compare)
     refresh()
 
 
 # Searching
-def binary_search(lst, target):
+def binary_search(lst, target, compare):
     left = 0
     right = len(lst) - 1
-
     while left <= right:
         mid = (left + right) // 2
-        if lst[mid][0] == target:
-            return mid
-        elif lst[mid][0] < target:
+        print(lst[mid][0])
+        if compare(lst[mid][0], target):
             left = mid + 1
-        else:
+        elif compare(target, lst[mid][0]):
             right = mid - 1
+        else:
+            return mid
 
-    return None
+    return -1
 
 def search_kelas(query):
-    bubble_sort(kelas, compare_kelas_ascending)
-    result = binary_search(kelas, query)
-    if result:
-        show_kelas_detail(kelas[result])
+    if is_acending:
+        bubble_sort(kelas, compare_kelas_ascending)
+        result = binary_search(kelas, query,compare_kelas_descending)
+    else:
+        bubble_sort(kelas, compare_kelas_descending)
+        result = binary_search(kelas, query,compare_kelas_ascending)
+    if result >= 0:
+        show_kelas_search(result+1,kelas[result])
     else:
         messagebox.showinfo("Not Found", "Kelas tidak ditemukan")
 
-
 def search_user(query):
-    bubble_sort(user, compare_user_ascending)
-    result = binary_search(user, query)
-    if result:
-        show_user_detail(user[result])
+    if is_acending:
+        bubble_sort(user, compare_user_ascending)
+        result = binary_search(user, query,compare_user_descending)
+    else:
+        bubble_sort(user, compare_user_descending)
+        result = binary_search(user, query,compare_user_ascending)
+    if result >= 0:
+        show_user_search(result+1,user[result])
     else:
         messagebox.showinfo("Not Found", "User tidak ditemukan")
 
 
 
-# Refresh
+# Tampilan Halaman Kelas
 def refresh_kelas():
     for widget in kelas_page.winfo_children():
         widget.destroy()
@@ -120,7 +148,7 @@ def refresh_kelas():
         kelas_button = tk.Button(kelas_frame, text="{}. {}".format(i+1, kelas[i][0]), anchor="w", font=("Arial", 14), relief="groove", padx=10, pady=5, width=20, borderwidth=4, bd=1, bg="white", fg="black", highlightthickness=0,  highlightcolor="white", command=lambda kelas_detail=kelas[i]: show_kelas_detail(kelas_detail))
         kelas_button.pack(side="left", pady=5)
 
-        join_button = tk.Button(kelas_frame, text="Join Class", font=("Arial", 16), relief="groove", padx=5, pady=2, width=10, borderwidth=2, bd=1, bg="blue", fg="white", highlightthickness=0,  highlightcolor="white", command=show_join_class_form)
+        join_button = tk.Button(kelas_frame, text="Join Class", font=("Arial", 16), relief="groove", padx=5, pady=2, width=10, borderwidth=2, bd=1, bg="blue", fg="white", highlightthickness=0,  highlightcolor="white", command=lambda: show_join_class_form(kelas[i]))
         join_button.pack(side="left", pady=5)
 
     sort_asc_button = tk.Button(kelas_page, text="Sort Ascending", font=("Arial", 18), relief="groove", padx=5, pady=2, width=15, borderwidth=2, bd=1, bg="green", fg="white", highlightthickness=0,  highlightcolor="white", command=lambda: sort_and_refresh(kelas, compare_kelas_ascending, refresh_kelas))
@@ -129,7 +157,7 @@ def refresh_kelas():
     sort_desc_button = tk.Button(kelas_page, text="Sort Descending", font=("Arial", 18), relief="groove", padx=5, pady=2, width=15, borderwidth=2, bd=1, bg="red", fg="white", highlightthickness=0,  highlightcolor="white", command=lambda: sort_and_refresh(kelas, compare_kelas_descending, refresh_kelas))
     sort_desc_button.pack(pady=5)
 
-
+# Tampilan Halaman User
 def refresh_user():
     for widget in user_page.winfo_children():
         widget.destroy()
@@ -168,6 +196,7 @@ def refresh_user():
     sort_desc_button = tk.Button(user_page, text="Sort Descending", font=("Arial", 18), relief="groove", padx=5, pady=2, width=15, borderwidth=2, bd=1, bg="red", fg="white", highlightthickness=0,  highlightcolor="white", command=lambda: sort_and_refresh(user, compare_user_descending, refresh_user))
     sort_desc_button.pack(pady=5)
 
+# Tampilan Halaman User Kelas
 def refresh_user_kelas():
     for widget in user_kelas_page.winfo_children():
         widget.destroy()
@@ -182,68 +211,73 @@ def refresh_user_kelas():
         kelas_button = tk.Button(kelas_frame, text="{}. {}".format(i+1, kelas[i][0]), anchor="w", font=("Arial", 14), relief="groove", padx=10, pady=5, width=20, borderwidth=4, bd=1, bg="white", fg="black", highlightthickness=0,  highlightcolor="white", command=lambda kelas_detail=kelas[i]: show_kelas_detail(kelas_detail))
         kelas_button.pack(side="left", pady=5)
 
-        join_button = tk.Button(kelas_frame, text="Show Users", font=("Arial", 16), relief="groove", padx=5, pady=2, width=10, borderwidth=2, bd=1, bg="yellow", fg="black", highlightthickness=0,  highlightcolor="white", command=show_join_class_form)
+        join_button = tk.Button(kelas_frame, text="Show Users", font=("Arial", 16), relief="groove", padx=5, pady=2, width=10, borderwidth=2, bd=1, bg="yellow", fg="black", highlightthickness=0,  highlightcolor="white", command=lambda kelas=kelas[i][0]: show_user_kelas(kelas))
         join_button.pack(side="left", pady=5)
 
-    sort_asc_button = tk.Button(user_kelas_page, text="Sort Ascending", font=("Arial", 18), relief="groove", padx=5, pady=2, width=15, borderwidth=2, bd=1, bg="green", fg="white", highlightthickness=0,  highlightcolor="white", command=lambda: sort_and_refresh(kelas, compare_kelas_ascending, refresh_kelas))
-    sort_asc_button.pack(pady=5)
+    
+           
 
-    sort_desc_button = tk.Button(user_kelas_page, text="Sort Descending", font=("Arial", 18), relief="groove", padx=5, pady=2, width=15, borderwidth=2, bd=1, bg="red", fg="white", highlightthickness=0,  highlightcolor="white", command=lambda: sort_and_refresh(kelas, compare_kelas_descending, refresh_kelas))
-    sort_desc_button.pack(pady=5)        
+# Form untuk mengikuti kelas
+def show_join_class_form(kelas):
+    form_window = Toplevel(kelas_page)
+    form_window.title(f"Join {kelas[0]}")
 
-#Form
-def show_join_class_form():
-        form_window = Toplevel(kelas_page)
-        form_window.title("Join Class Form")
+    # Create form labels
+    name_label = tk.Label(form_window, text="Nama Anda:")
+    name_label.grid(row=1, column=0, padx=10, pady=5, sticky="w")
+    name_entry = tk.Entry(form_window)
+    name_entry.grid(row=1, column=1, padx=10, pady=5)
 
-        # Create form labels
-        name_label = tk.Label(form_window, text="Nama Anda:")
-        name_label.grid(row=1, column=0, padx=10, pady=5, sticky="w")
-        name_entry = tk.Entry(form_window)
-        name_entry.grid(row=1, column=1, padx=10, pady=5)
+    email_label = tk.Label(form_window, text="Email Anda:")
+    email_label.grid(row=2, column=0, padx=10, pady=5, sticky="w")
+    email_entry = tk.Entry(form_window)
+    email_entry.grid(row=2, column=1, padx=10, pady=5)
 
-        email_label = tk.Label(form_window, text="Email Anda:")
-        email_label.grid(row=2, column=0, padx=10, pady=5, sticky="w")
-        email_entry = tk.Entry(form_window)
-        email_entry.grid(row=2, column=1, padx=10, pady=5)
+    phone_label = tk.Label(form_window, text="Nomor Telephone:")
+    phone_label.grid(row=3, column=0, padx=10, pady=5, sticky="w")
+    phone_entry = tk.Entry(form_window)
+    phone_entry.grid(row=3, column=1, padx=10, pady=5)
 
-        phone_label = tk.Label(form_window, text="Nomor Telephone:")
-        phone_label.grid(row=3, column=0, padx=10, pady=5, sticky="w")
-        phone_entry = tk.Entry(form_window)
-        phone_entry.grid(row=3, column=1, padx=10, pady=5)
+    submit_button = tk.Button(form_window, text="Submit", command=lambda: submit_form(name_entry.get(), email_entry.get(),phone_entry.get(), kelas[0]))
+    submit_button.grid(row=4, column=0, columnspan=2, pady=10)
 
-        # Create submit button
-        submit_button = tk.Button(form_window, text="Submit", command=lambda: submit_form(name_entry.get(), email_entry.get(),phone_entry.get()))
-        submit_button.grid(row=4, column=0, columnspan=2, pady=10)
-
-def submit_form(name, email, phone):
+# Submit form
+def submit_form(name, email, phone, kelas):
     user.append([name, email, phone])
+    user_kelas.append([name, kelas])
     messagebox.showinfo("Berhasil", "Selamat anda telah berhasil mendafar ke kelas ini!")
     sort_and_refresh(user, compare_user_ascending, refresh_user)
 
 
-# Detail
+# Details
 def show_kelas_detail(kelas_detail):
-    messagebox.showinfo("Kelas Detail", f"Nama Kelas: {kelas_detail[0]}\nMentor: {kelas_detail[1]}")
+    messagebox.showinfo("Kelas Detail", f"Nama Kelas: {kelas_detail[0]}\nMentor: {kelas_detail[1]}\nDurasi: {kelas_detail[2]} bulan")
+
+def show_kelas_search(index,kelas_detail):
+    messagebox.showinfo(f"Kelas Ditemukan di Posisi {index}", f"Nama Kelas: {kelas_detail[0]}\nMentor: {kelas_detail[1]}\nDurasi: {kelas_detail[2]} bulan")
 
 def show_user_detail(user_detail):
     messagebox.showinfo("User Detail", f"Nama: {user_detail[0]}\nEmail: {user_detail[1]}\nNomor: {user_detail[2]}")
 
-# Fungsi
+def show_user_search(index,user_detail):
+    messagebox.showinfo(f"User Ditemukan di Posisi {index}", f"Nama: {user_detail[0]}\nEmail: {user_detail[1]}\nNomor: {user_detail[2]}")
+
+def show_user_kelas(kelas):
+    filtered_users = []
+    for user in user_kelas:
+        if user[1] == kelas:
+            filtered_users.append(user[0])
+    
+    user_list = "\n".join(["{}. {}".format(i+1, user) for i, user in enumerate(filtered_users)])
+    if len(filtered_users) == 0:
+        messagebox.showinfo("Filtered Users", "Tidak ada user yang terdaftar di kelas ini")
+    else:    
+        messagebox.showinfo("Filtered Users", user_list)
+
+
+# Fungsi tambahan untuk menampilkan halaman
 def show_page(notebook,page):
     notebook.select(page)
-
-
-#Tampilan
-
-
-
-def tampilkan_user_kelas_gui(tk,page):
-    for i in range(len(user_kelas)):
-        user_kelas_label = tk.Label(page, text="{}. {}".format(i+1, user_kelas[i][0]))
-        user_kelas_label.pack()
-
-
 
 def tampilkan_menu_gui():
     notebook.add(kelas_page, text='Daftar Kelas')
@@ -255,6 +289,7 @@ def tampilkan_menu_gui():
     root.mainloop()
 
 
-#main
+# Memanggil fungsi
+sort_and_refresh(kelas, compare_kelas_ascending, refresh_kelas)
+sort_and_refresh(user, compare_user_ascending, refresh_user)
 tampilkan_menu_gui()
-# search_kelas()
